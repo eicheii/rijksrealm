fef1b8d1bfb591c4e169d53d2dcff8e0bf4da50a
eicheii <hiba.awni02@gmail.com>
2024-05-21 11:19:25 +0200

    saved artworks link

--- rijksrealm/src/components/header.js
+++ rijksrealm/src/components/header.js
@@ -1,12 +1,32 @@
+// header.js
 import React from "react";
 
 const Header = () => {
     return (
-        <header>
-            <h1> Rijksrealm</h1>
-            {/* navigation link: or list */}
+        <header className="header">
+            <h1 className="header-title">Rijksrealm</h1>
+            <nav className="header-nav">
+                <a href="#my-gallery" className="header-link">Saved Artworks</a>
+            </nav>
         </header>
     );
 };
 
 export default Header;
+
+/* nav suggestion?
+
+{/*  const Header = () => {
+    return (
+        <header>
+            <h1> Rijksrealm</h1>
+            <nav>
+                <ul>
+                    <li><a href ="#Home"</a></li>"
+                    <li><a href ="#My Gallery" </a></li>"
+            </nav>
+            </header> 
+        );
+    };  
+
+*/
